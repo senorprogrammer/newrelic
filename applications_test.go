8fb1656f4f9f8c1138f780bf0275aa97d728c8b2
Kurt Scherer <kscherer@CLI-15985.local>
2016-08-27 16:25:28 -0500

    Adding GetApplication test

---
+++ applications_test.go
@@ -0,0 +1,147 @@
+package newrelic
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+	"time"
+)
+
+const (
+	testApiKey          = "test_api_key"
+	fixtureDir          = "./test-fixtures"
+	testApplicationJson = `
+{
+  "application": {
+    "application_summary": {
+      "apdex_score": 1.0,
+      "apdex_target": 0.5,
+      "error_rate": 0.0,
+      "host_count": 1,
+      "instance_count": 1,
+      "response_time": 0.263,
+      "throughput": 12.3
+    },
+    "end_user_summary": {
+      "response_time": 0.263,
+      "throughput": 12.3,
+      "apdex_target": 0.5,
+      "apdex_score": 1
+    },
+    "health_status": "green",
+    "id": 12345,
+    "language": "java",
+    "last_reported_at": "2016-01-20T20:29:38+00:00",
+    "links": {
+      "alert_policy": 123,
+      "application_hosts": [
+        1234567
+      ],
+      "application_instances": [
+        1234568
+      ],
+      "servers": [
+        54321
+      ]
+    },
+    "name": "test.example.com",
+    "reporting": true,
+    "settings": {
+      "app_apdex_threshold": 0.5,
+      "enable_real_user_monitoring": true,
+      "end_user_apdex_threshold": 1.0,
+      "use_server_side_config": false
+    }
+  },
+  "links": {
+    "application.alert_policy": "/v2/alert_policies/{alert_policy_id}",
+    "application.application_host": "/v2/application/{application_id}/hosts/{host_id}",
+    "application.application_hosts": "/v2/application/{application_id}/hosts?ids={host_ids}",
+    "application.application_instance": "/v2/application/{application_id}/instances/{instance_id}",
+    "application.application_instances": "/v2/application/{application_id}/instances?ids={instance_ids}",
+    "application.server": "/v2/servers/{server_id}",
+    "application.servers": "/v2/servers?ids={server_ids}"
+  }
+}
+`
+)
+
+var testApplication = &Application{
+	Id:             12345,
+	Name:           "test.example.com",
+	Language:       "java",
+	HealthStatus:   "green",
+	Reporting:      true,
+	LastReportedAt: time.Date(2016, 1, 20, 20, 29, 38, 0, time.FixedZone("", 0)),
+	ApplicationSummary: ApplicationSummary{
+		ResponseTime:            0.263,
+		Throughput:              12.3,
+		ErrorRate:               0,
+		ApdexTarget:             0.5,
+		ApdexScore:              1,
+		HostCount:               1,
+		InstanceCount:           1,
+		ConcurrentInstanceCount: 0,
+	},
+	EndUserSummary: EndUserSummary{
+		ResponseTime: 0.263,
+		Throughput:   12.3,
+		ApdexTarget:  0.5,
+		ApdexScore:   1,
+	},
+	Settings: Settings{
+		AppApdexThreshold:        0.5,
+		EndUserApdexThreshold:    1,
+		EnableRealUserMonitoring: true,
+		UseServerSideConfig:      false,
+	},
+	Links: Links{
+		Servers:              []int{54321},
+		ApplicationHosts:     []int{1234567},
+		ApplicationInstances: []int{1234568},
+		AlertPolicy:          123,
+	},
+}
+
+type getApplicationInput struct {
+	id   int
+	data string
+}
+
+type getApplicationOutput struct {
+	data *Application
+	err  error
+}
+
+var getApplicationTests = []struct {
+	in  getApplicationInput
+	out getApplicationOutput
+}{
+	{
+		getApplicationInput{
+			id:   12345,
+			data: testApplicationJson,
+		},
+		getApplicationOutput{
+			data: testApplication,
+		},
+	},
+}
+
+func TestGetApplication(t *testing.T) {
+	t.Logf("Starting TestGetApplication")
+	for _, tt := range getApplicationTests {
+		t.Logf("Testing")
+		h := func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(200)
+			fmt.Fprintf(w, tt.in.data)
+		}
+		c, s := initHttp(t, testApiKey, h)
+		defer s.Close()
+		resp, err := c.GetApplication(tt.in.id)
+		t.Logf("Checking err...")
+		expect(t, tt.out.err, err)
+		t.Logf("Checking output...")
+		expect(t, tt.out.data, resp)
+	}
+}
---
+++ common_test.go
@@ -0,0 +1,57 @@
+package newrelic
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"path"
+	"reflect"
+	"testing"
+
+	"github.com/kr/pretty"
+)
+
+func expect(t *testing.T, exp interface{}, got interface{}) {
+	if !reflect.DeepEqual(got, exp) {
+		e := pretty.Sprint(exp)
+		g := pretty.Sprint(got)
+		t.Errorf("Want (type %v): \n---\n\"%s\"\n---\n", reflect.TypeOf(exp), e)
+		t.Errorf("Got (type %v): \n---\n\"%s\"\n---\n", reflect.TypeOf(got), g)
+	} else {
+		t.Logf("Expectation matches.")
+	}
+}
+
+type RewriteTransport struct {
+	URL *url.URL
+}
+
+func (t RewriteTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	// Maintain the opaque path, but rewrite the base URL
+	req.URL.Scheme = t.URL.Scheme
+	req.URL.Host = t.URL.Host
+	req.URL.Path = path.Join(t.URL.Path, req.URL.Path)
+	return http.DefaultTransport.RoundTrip(req)
+}
+
+type HandlerFunc func(http.ResponseWriter, *http.Request)
+
+func handlerFactory(code int) HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(code)
+		fmt.Fprintf(w, "%s ", r.Method)
+		fmt.Fprintf(w, "%s %s %s", r.Host, r.URL.Path, r.URL.Query().Encode())
+	}
+}
+
+func initHttp(t *testing.T, apiKey string, f HandlerFunc) (*Client, *httptest.Server) {
+	mockServer := httptest.NewServer(http.HandlerFunc(f))
+	u, err := url.Parse(mockServer.URL)
+	if err != nil {
+		t.Log("Problem initiating mock http server: ", err)
+	}
+	c := NewClient(apiKey)
+	c.httpClient = &http.Client{Transport: RewriteTransport{URL: u}}
+	return c, mockServer
+}
