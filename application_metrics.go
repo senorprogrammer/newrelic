106f677545e08228a02a402eafb138cf2c30bb6e
Kristinn Örn Sigurðsson <kos@root.is>
2017-05-02 16:03:22 +0200

    Feature: Component metrics support.
    1. List metrics.
    2. Fetch metrics data.
    
    I did some refactoring to get rid of code duplication.
    
    The component metrics code doesn't need any extra coding since it's
    using the same code base as the application metrics (after the
    refactoring).
    
    This commit solves the following issues:
    https://github.com/yfronto/newrelic/issues/54
    https://github.com/yfronto/newrelic/issues/55

--- application_metrics.go
+++ application_metrics.go
@@ -1,39 +1,36 @@
 package newrelic
 
 import (
-	"strconv"
+	"fmt"
 )
 
 // GetApplicationMetrics will return a slice of Metric items for a
 // particular Application ID, optionally filtering by
 // MetricsOptions.
 func (c *Client) GetApplicationMetrics(id int, options *MetricsOptions) ([]Metric, error) {
-	resp := &struct {
-		Metrics []Metric `json:"metrics,omitempty"`
-	}{}
-	path := "applications/" + strconv.Itoa(id) + "/metrics.json"
-	err := c.doGet(path, options, resp)
-	if err != nil {
-		return nil, err
-	}
-	return resp.Metrics, nil
+	mc := NewMetricClient(c)
+
+	return mc.GetMetrics(
+		fmt.Sprintf(
+			"applications/%d/metrics.json",
+			id,
+		),
+		options,
+	)
 }
 
 // GetApplicationMetricData will return all metric data for a particular
 // application and slice of metric names, optionally filtered by
 // MetricDataOptions.
 func (c *Client) GetApplicationMetricData(id int, names []string, options *MetricDataOptions) (*MetricDataResponse, error) {
-	resp := &struct {
-		MetricData MetricDataResponse `json:"metric_data,omitempty"`
-	}{}
-	if options == nil {
-		options = &MetricDataOptions{}
-	}
-	options.Names = Array{names}
-	path := "applications/" + strconv.Itoa(id) + "/metrics/data.json"
-	err := c.doGet(path, options, resp)
-	if err != nil {
-		return nil, err
-	}
-	return &resp.MetricData, nil
+	mc := NewMetricClient(c)
+
+	return mc.GetMetricData(
+		fmt.Sprintf(
+			"applications/%d/metrics/data.json",
+			id,
+		),
+		names,
+		options,
+	)
 }
--- server_metrics.go
+++ server_metrics.go
@@ -1,37 +1,34 @@
 package newrelic
 
 import (
-	"strconv"
+	"fmt"
 )
 
 // GetServerMetrics will return a slice of Metric items for a particular
 // Server ID, optionally filtering by MetricsOptions.
 func (c *Client) GetServerMetrics(id int, options *MetricsOptions) ([]Metric, error) {
-	resp := &struct {
-		Metrics []Metric `json:"metrics,omitempty"`
-	}{}
-	path := "servers/" + strconv.Itoa(id) + "/metrics.json"
-	err := c.doGet(path, options, resp)
-	if err != nil {
-		return nil, err
-	}
-	return resp.Metrics, nil
+	mc := NewMetricClient(c)
+
+	return mc.GetMetrics(
+		fmt.Sprintf(
+			"servers/%d/metrics.json",
+			id,
+		),
+		options,
+	)
 }
 
 // GetServerMetricData will return all metric data for a particular Server and
 // slice of metric names, optionally filtered by MetricDataOptions.
 func (c *Client) GetServerMetricData(id int, names []string, options *MetricDataOptions) (*MetricDataResponse, error) {
-	resp := &struct {
-		MetricData MetricDataResponse `json:"metric_data,omitempty"`
-	}{}
-	if options == nil {
-		options = &MetricDataOptions{}
-	}
-	options.Names = Array{names}
-	path := "servers/" + strconv.Itoa(id) + "/metrics/data.json"
-	err := c.doGet(path, options, resp)
-	if err != nil {
-		return nil, err
-	}
-	return &resp.MetricData, nil
+	mc := NewMetricClient(c)
+
+	return mc.GetMetricData(
+		fmt.Sprintf(
+			"servers/%d/metrics/data.json",
+			id,
+		),
+		names,
+		options,
+	)
 }
